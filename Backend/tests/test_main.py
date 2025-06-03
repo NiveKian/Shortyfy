d37a00aed262e71f851c41fc250efa9457a274be
NiveKian <ian1swipe@gmail.com>
2025-06-03 18:24:23 -0300

    Add: started shortyfy project
    Project to shortend web links
    with an python API
    And a React + Tailwind frontend

---
+++ Backend/tests/test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from main import app  # import your FastAPI app
+
+client = TestClient(app)
+
+def test_shorten_url():
+    response = client.post("/shortyfy", json={"url": "https://example.com"})
+    assert response.status_code == 200
+    data = response.json()
+    assert "short_url" in data
+    assert data["short_url"].startswith("http")
+
+def test_redirect_url_not_found():
+    response = client.get("/nonexistentid")
+    assert response.status_code == 404
---
+++ README.md
@@ -0,0 +1,12 @@
+# Estrutura Backend
+
+backend/
+├── tests/
+│   ├── __init__.py
+│   └── test_url.py
+├── .env
+├── .env.example
+├── main.py
+└── requirements.txt
+
+# Estrutura Frontend
