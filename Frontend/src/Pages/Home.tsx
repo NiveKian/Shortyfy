0cb63cfeb6a308302db9869d0fdfac9a5669b963
NiveKian <ian1swipe@gmail.com>
2025-09-23 13:38:52 -0300

    Fix: vite import.meta.env usage

--- Frontend/src/Pages/Home.tsx
+++ Frontend/src/Pages/Home.tsx
@@ -12,7 +12,7 @@ export const Home = () => {
 
   const [url, setUrl] = useState("")
   const navigate = useNavigate()
-  const API_URL = import.meta.env.SHORTYFY_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const handleSubmit = async () => {
     try {
